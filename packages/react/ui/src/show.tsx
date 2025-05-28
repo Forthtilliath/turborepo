f752eb60a9c70f7448bf3c14f6490142d91bef6b
Forthtilliath <forth@live.fr>
2025-05-28 02:55:44 +0200

    chore: add custom packages

---
+++ packages/react/ui/src/show.tsx
@@ -0,0 +1,46 @@
+type Props<T> = {
+	when: T;
+	fallback?: React.ReactNode;
+	children: React.ReactNode | ((whenValue: NonNullable<T>) => React.ReactNode);
+};
+
+/**
+ * A conditional component that renders its `children` when the `when` property
+ * is truthy. If `when` is falsy, renders the `fallback` property or `null` if
+ * `fallback` is not supplied.
+ *
+ * The `children` property can be either a React node or a function. If the
+ * `children` property is a function, it is called with the value of the `when`
+ * property as its argument.
+ *
+ * @example
+ * <Show when={user.name}>
+ *   <p>Welcome, {user.name}!</p>
+ * </Show>
+ *
+ * @example
+ * <Show when={user.name}>
+ *   {(name) => <p>Welcome, {name}!</p>}
+ * </Show>
+ *
+ * @param {Props<T>} props
+ * @returns {React.ReactNode}
+ */
+export function Show<T>({
+	children,
+	when,
+	fallback = null,
+}: Props<T>): React.ReactNode {
+	if (when) {
+		const wrappedChildren = () => {
+			if (typeof children === "function") {
+				return children(when);
+			}
+			return children;
+		};
+
+		return wrappedChildren();
+	}
+
+	return fallback;
+}
