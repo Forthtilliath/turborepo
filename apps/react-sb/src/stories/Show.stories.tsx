e4c9395bc784254610343af1d5724642b2128d55
Forthtilliath <forth@live.fr>
2025-05-28 03:54:57 +0200

    chore: clean up sb folder

---
+++ apps/react-sb/src/stories/Show.stories.tsx
@@ -0,0 +1,39 @@
+import { Show } from "@repo/react-ui/show";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+	title: "Ui/Show",
+	component: Show,
+	parameters: {
+		layout: "centered",
+	},
+	tags: ["autodocs"],
+	argTypes: {
+		when: { control: "boolean" },
+	},
+	render: ({ when, fallback, children }) => (
+		<Show when={when} fallback={fallback}>
+			{children}
+		</Show>
+	),
+} satisfies Meta<typeof Show>;
+
+export default meta;
+type Story = StoryObj<typeof Show>;
+
+export const Default: Story = {
+	args: {
+		when: true,
+		fallback: <div>Pas d&apos;élément à afficher</div>,
+		children: (
+			<div>
+				<p>Liste des éléments disponibles</p>
+				<ul>
+					<li>Item 1</li>
+					<li>Item 2</li>
+					<li>Item 3</li>
+				</ul>
+			</div>
+		),
+	},
+};
