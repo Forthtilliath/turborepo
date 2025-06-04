1b44b0964ad542c454ca4d99d4a181b6705ffcd0
Forthtilliath <forth@live.fr>
2025-06-04 21:20:06 +0200

    fix: add exports

--- packages/react/ui/src/index.ts
+++ packages/react/ui/src/index.ts
@@ -1,1 +1,3 @@
-export { Show }  from "./show";
+export { Repeat, type RepeatProps }  from "./repeat";
+export { Show, type ShowProps }  from "./show";
+export { SlotOrCallback, type SlotOrCallbackProps }  from "./slot-or-callback";
