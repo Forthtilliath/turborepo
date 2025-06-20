28af3b643031b697275e243326386756e4459fe5
Forthtilliath <forth@live.fr>
2025-06-20 17:03:58 +0200

    build: setup config for shadcn-ui stories

--- apps/react-sb/.storybook/preview.ts
+++ apps/react-sb/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react'
+import '@forthtilliath/shadcn-ui/globals.css'
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +10,8 @@ const preview: Preview = {
       },
     },
   },
+
+  tags: ["autodocs"],
 };
 
 export default preview;
