c6d1a9953d23f5f875db22e136a364f5b32f6730
Forthtilliath <forth@live.fr>
2025-06-20 17:32:30 +0200

    build: configure storybook docs

--- apps/react-sb/.storybook/main.ts
+++ apps/react-sb/.storybook/main.ts
@@ -22,11 +22,15 @@ const config: StorybookConfig = {
       }
     },
     getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath('@storybook/addon-interactions')
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-docs'),
   ],
   "framework": {
     "name": getAbsolutePath('@storybook/react-vite'),
     "options": {}
+  },
+  docs: {
+    defaultName: 'Documentation',
   }
 };
 export default config;
