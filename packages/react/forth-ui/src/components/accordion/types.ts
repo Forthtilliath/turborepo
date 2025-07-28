b3ebf6f81dfa8791931017dd363c8213777b374c
Forthtilliath <forth@live.fr>
2025-07-28 23:09:20 +0200

    chore: add jsdoc on props

--- packages/react/forth-ui/src/components/accordion/types.ts
+++ packages/react/forth-ui/src/components/accordion/types.ts
@@ -31,27 +31,92 @@ interface ItemWithIcon extends Item {
 }
 
 interface BaseProps {
+  /**
+   * The items to render in the accordion.
+   */
   items: Item[] | ItemWithIcon[];
+  /**
+   * The icon displayed next to the title is the same for all items.
+   * 
+   * Additionally, using both ItemWithIcon and icon will throw an error.
+   */
   icon?: React.ReactNode;
-  multiple?: boolean;
+  /**
+   * Hide the chevron icon.
+   * @default false
+   */
   hideChevron?: boolean;
+  /**
+   * The alignment of the chevron icon.
+   * @default "right"
+   */
   chevronAlignment?: VariantProps<
     typeof accordionTriggerVariants
   >["chevronAlignment"];
+  /**
+   * Custom chevron icon.
+   */
   customChevron?: React.ReactElement<LucideProps>;
+  /**
+   * Style the accordion.
+   */
   className?: string;
+  /**
+   * Style the accordion item.
+   */
   classNameItem?: string;
+  /**
+   * Style the accordion trigger.
+   */
   classNameTrigger?: string;
+  /**
+   * Style the accordion content.
+   */
   classNameContent?: string;
+  /**
+   * The variant of the accordion.
+   * @default "default"
+   */
   variant?: AccordionVariantType;
+  /**
+   * The size of the accordion.
+   * @default "default"
+   */
   size?: AccordionSizeType;
+  /**
+   * Generate a unique id for each item.
+   * @default `item-{index}`
+   */
   generateId?: (index: number) => string;
 }
 
 // Props removed from Primitive props
-type OmittedProps = "type";
-export type SingleProps = BaseProps & Omit<AccordionSingleProps, OmittedProps>;
+type OmittedProps = "type" | "defaultValue" | "collapsible";
+export type SingleProps = BaseProps &
+  Omit<AccordionSingleProps, OmittedProps> & {
+    /**
+     * Set the ``type`` prop to ``multiple`` to enable opening multiple items at once.
+     * @default false
+     */
+    multiple?: false;
+    /** Set the default open item */
+    defaultValue?: string;
+    /**
+     * Use the ``collapsible`` prop to allow all items to close.
+     * @default false
+     */
+    collapsible?: boolean;
+  };
 export type MultipleProps = BaseProps &
-  Omit<AccordionMultipleProps, OmittedProps>;
+  Omit<AccordionMultipleProps, OmittedProps> & {
+    multiple: true;
+    /** Set the default open items */
+    defaultValue?: string[];
+    /**
+     * Use the ``collapsible`` prop to allow all items to close.
+     * @default true
+     */
+    collapsible?: true;
+  };
 
 export type AccordionProps = SingleProps | MultipleProps;
