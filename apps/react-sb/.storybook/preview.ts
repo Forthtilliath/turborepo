6f9fc76a78ed3d5da1c448f8d908b4550541dc3f
Forthtilliath <forth@live.fr>
2025-08-01 01:21:24 +0200

    chore: update styles

--- apps/react-sb/.storybook/preview.ts
+++ apps/react-sb/.storybook/preview.ts
@@ -1,6 +1,10 @@
 import type { Preview } from "@storybook/react-vite";
 
+// To be able to use all classes inside stories className inputs
+import "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4";
+
 import "@forthtilliath/shadcn-ui/globals.css";
+import "@forthtilliath/shadcn-ui/themes/default.css";
 
 const preview: Preview = {
   parameters: {
