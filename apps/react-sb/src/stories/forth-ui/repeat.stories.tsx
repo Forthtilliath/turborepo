076fd4a943530a35f93a81a5fe670229d1e2960c
Forthtilliath <forth@live.fr>
2025-08-25 00:31:08 +0200

    chore: update repeat story

--- apps/react-sb/src/stories/forth-ui/repeat.stories.tsx
+++ apps/react-sb/src/stories/forth-ui/repeat.stories.tsx
@@ -2,6 +2,9 @@ import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { Repeat } from "@forthtilliath/forth-ui/components/repeat";
 
+/**
+ * Repeat a component a specified number of times.
+ */
 const meta: Meta<typeof Repeat> = {
   title: "forth-ui/Repeat",
   component: Repeat,
@@ -49,22 +52,111 @@ const meta: Meta<typeof Repeat> = {
 export default meta;
 type Story = StoryObj<typeof Repeat>;
 
+/**
+ * Default repeat component.
+ */
 export const Default: Story = {
   args: {
     renderItem: () => <p>Message statique répété plusieurs fois</p>,
   },
+  parameters: {
+    docs: {
+      source: {
+        transform: (source: string) => {
+          return source.replace(
+            "renderItem={() => {}}",
+            "renderItem={() => <p>Message statique répété plusieurs fois</p>}"
+          );
+        },
+      },
+    },
+  },
 };
 
+/**
+ * Use ``renderItem`` prop to specify the content of each item.
+ */
 export const WithCallback: Story = {
   args: {
     renderItem: (i: number) => (
       <p>Message dynamique répété plusieurs fois, index {i}</p>
     ),
   },
+  parameters: {
+    docs: {
+      source: {
+        transform: (source: string) => {
+          return source.replace(
+            "renderItem={() => {}}",
+            [
+              "renderItem={(i: number) => (",
+              "    <p>Message dynamique répété plusieurs fois, index {i}</p>",
+              "  )}",
+            ].join("\n")
+          );
+        },
+      },
+    },
+  },
 };
 
+/**
+ * Use ``keyItem`` prop to specify the key of each item.
+ */
 export const WithCustomKey: Story = {
   args: {
     keyItem: (i: number) => `key-${i.toString()}`,
+  },
+  parameters: {
+    docs: {
+      source: {
+        transform: (source: string) => {
+          return source
+            .replace(
+              "keyItem={() => {}}",
+              "keyItem={(i: number) => `key-${i.toString()}`}"
+            )
+            .replace(
+              "renderItem={() => {}}",
+              [
+                "renderItem={() => (",
+                "    <p>Message statique répété plusieurs fois</p>",
+                "  )}",
+              ].join("\n")
+            );
+        },
+      },
+    },
+  },
+};
+
+function CustomComponent() {
+  return <p>Message statique dans un component</p>;
+}
+
+/**
+ * Use ``renderItem`` prop to render a custom component.
+ * 
+ * ```tsx
+ * function CustomComponent() {
+ *   return <p>Message statique dans un component</p>;
+ * }
+ * ```
+ */
+export const WithComponent: Story = {
+  args: {
+    renderItem: () => <CustomComponent />,
+  },
+  parameters: {
+    docs: {
+      source: {
+        transform: (source: string) => {
+          return source.replace(
+            "renderItem={() => {}}",
+            "renderItem={() => <CustomComponent />}"
+          );
+        },
+      },
+    },
   },
 };
