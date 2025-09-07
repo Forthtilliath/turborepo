fe0a84d0397ad8536a8c5c84a327f29d829b861e
Forthtilliath <forth@live.fr>
2025-09-07 22:33:34 +0200

    feat: add file methods

---
+++ packages/lib/src/files/downloadText.ts
@@ -0,0 +1,39 @@
+/**
+ * Downloads a text blob as a file.
+ * @param filename The filename to save to.
+ * @param text The text to save.
+ */
+export function downloadText(filename: string, text: string): void {
+  const element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+/**
+ * Downloads a text blob as a file.
+ * @param filename The filename to save to.
+ * @param text The text to save.
+ */
+export function downloadTextBlob(filename: string, text: string): void {
+  const url = URL.createObjectURL(new Blob([text], { type: "text/plain" }));
+
+  const a = document.createElement("a");
+  a.setAttribute("download", filename);
+  a.setAttribute("href", url);
+  a.style.setProperty("display", "none");
+  document.body.appendChild(a);
+  a.click();
+
+  URL.revokeObjectURL(url);
+  document.body.removeChild(a);
+}
